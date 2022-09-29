9375fe92c9fda53a3901f828a23a092ba7deebb3
Alan Jui <alanjui.1960@gmail.com>
2022-09-29 20:33:10 +0800

    (1) 開始啟用 Python Module ，以便程式碼結構化，不至有一條龍過長難以閱讀及維護程式碼的問題；(2) 啟用 PostgreSQL 資料庫，用於儲存漢字／注音碼，及查詢漢字的注音碼。

---
+++ tools/han_ji_khoo-import.sql
@@ -0,0 +1,4 @@
+COPY han_ji(han_ji, chu_im, freq, siann, un, tiau)
+FROM '/Users/alanjui/workspace/rime/ho-lok-oe-chu-im/nga_siok_thong_sip_ngoo_im.csv'
+DELIMITER ','
+CSV HEADER;
---
+++ tools/han_ji_khoo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE han_ji (
+  id SERIAL,
+  han_ji VARCHAR(6),
+  chu_im VARCHAR(15),
+  freq REAL,
+  siann VARCHAR(3),
+  un VARCHAR(5),
+  tiau INTEGER,
+  PRIMARY KEY (id)
+)
