940ab60294d34d0f817039925a15dcde13063af7
Alan Jui <alanjui.1960@gmail.com>
2022-11-29 17:44:38 +0800

    建立初步 python code

---
+++ python/m100.py
@@ -0,0 +1,10 @@
+from p01_import_source_data import main_run as san_sing_han_ji_tsu_im_paiau
+from p100_cha_ji_tian import main_run as cha_ji_tian
+from p210_hoo_goa_chu_im_all import main_run as hoo_goa_chu_im_all
+
+CONVERT_FILE_NAME = 'hoo-goa-chu-im.xlsx'
+
+
+san_sing_han_ji_tsu_im_paiau(CONVERT_FILE_NAME)
+cha_ji_tian(CONVERT_FILE_NAME)
+hoo_goa_chu_im_all(CONVERT_FILE_NAME)
